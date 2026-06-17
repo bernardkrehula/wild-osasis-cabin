8a5cbf9d7be8ee0aca10cd447af4343b190d4475
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-17 18:16:49 +0200

    feat: add logo content to <NavBar> component

--- src/layouts/navbar/index.tsx
+++ src/layouts/navbar/index.tsx
@@ -1,10 +1,13 @@
-import './index.css';
+import "./index.css";
 
 const NavBar = () => {
-    return(
-        <div>
-
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <div className="logo-content">
+        <img src="./logo2.png" />
+        <h1>Alpine retreat</h1>
+      </div>
+    </div>
+  );
+};
 export default NavBar;
