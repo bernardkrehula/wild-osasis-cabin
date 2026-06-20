dbaf36d6b7851ea70ed6d275e9be1541d4822fbc
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-20 20:03:45 +0200

    feat: add handleActiveIcon function to <Apartments> component

--- src/pages/apartments/index.tsx
+++ src/pages/apartments/index.tsx
@@ -1,10 +1,76 @@
-import './index.css';
+import { useState } from "react";
+import Btn from "../../components/ui/btn";
+import Select from "../../components/ui/select";
+import { aparmnetsDiscount, apartmentsSort } from "../../config/sortConfig";
+import "./index.css";
 
 const Apartments = () => {
-    return(
-        <div>
+  const [activeIcon, setActiveIcon] = useState({
+    all: false,
+    noDiscount: false,
+    withDiscount: false,
+  });
 
-        </div>
-    )
-}
+  const handleActiveIcon = (e: React.ChangeEvent<HTMLButtonElement>) => {
+    const name = e.target.name;
+    setActiveIcon((prev) => {
+      const active = Object.fromEntries(
+        Object.entries(prev).map(([icon]) =>
+          icon === name ? [name, true] : [icon, false],
+        ),
+      );
+      return active as typeof activeIcon;
+    });
+  };
+  return (
+    <div className="apartments">
+      <div className="apartments-header">
+        <h1>All apartments</h1>
+        <menu>
+          {aparmnetsDiscount.map((discount) => {
+            const { name, content } = discount;
+            return (
+              <Btn
+                key={name}
+                variation="ghost"
+                size="md"
+                active={`${activeIcon && activeIcon[name] && "act"}`}
+                name={name}
+                onClick={handleActiveIcon}
+              >
+                {content}
+              </Btn>
+            );
+          })}
+        </menu>
+        <Select options={apartmentsSort} size="md" />
+      </div>
+      <table className="apartments-table">
+        <thead className="apartments-table-header">
+          <tr>
+            <th>Cabin</th>
+            <th>Capacity</th>
+            <th>Price</th>
+            <th>Discount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Apartment 1</td>
+            <td>100€</td>
+            <td>4</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          {" "}
+          {/* optional - totals, summaries */}
+          <tr>
+            <td>Total</td>
+            <td>100€</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+};
 export default Apartments;
