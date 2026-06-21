4023d3c3fcf1b40dd466335673c499737afbc52b
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-21 19:53:35 +0200

    refactor: change <Dashboard> return structure

--- src/layouts/dasboard/index.tsx
+++ src/layouts/dasboard/index.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../navbar";
 import "./index.css";
+import Header from "../header";
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <NavBar />
-      <Outlet />
+      <div className="dashboard-content">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
