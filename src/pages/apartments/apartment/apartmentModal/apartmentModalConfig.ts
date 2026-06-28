a2f77bd243479c11764a1b0926f9ac35422dcb5f
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-28 20:13:07 +0200

    feat: add apartmentModalConfig file

---
+++ src/pages/apartments/apartment/apartmentModal/apartmentModalConfig.ts
@@ -0,0 +1,38 @@
+export const apartmentModalConfig = [
+  {
+    id: "apartment-name",
+    label: "Apartment name",
+    type: "text",
+    name: "name",
+  },
+  {
+    id: "maximum-capacity",
+    label: "Maximum capacity",
+    type: "number",
+    name: "capacity",
+  },
+  {
+    id: "regular-price",
+    label: "Regular price",
+    type: "number",
+    name: "price",
+  },
+  {
+    id: "discount",
+    label: "Discount",
+    type: "number",
+    name: "discount",
+  },
+  {
+    id: "description",
+    label: "Description for website",
+    type: "textarea",
+    name: "description",
+  },
+  {
+    id: "cabin-photo",
+    label: "Cabin photo",
+    type: "file",
+    name: "img",
+  },
+];
