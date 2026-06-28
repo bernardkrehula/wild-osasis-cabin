279df2a1af8efef0af921422086d2b9396d95b91
Bernard Krehula <krehula.bernard456@gmail.com>
2026-06-28 20:19:34 +0200

    feat: add apartment menu handle functions

--- src/pages/apartments/index.tsx
+++ src/pages/apartments/index.tsx
@@ -8,22 +8,35 @@ import {
 import "./index.css";
 import Apartment from "./apartment";
 import { useSearchParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { requestApartments } from "#/api/requestApartmets";
-import { requestEditApartment } from "#/api/requestEditApartment";
+import type { ActiveIcon } from "#/types/pagest.types.ts/ApartmentPage.types.ts/ActiveIcon.type";
+import type { ApartmentType } from "#/types/pagest.types.ts/ApartmentPage.types.ts/Apartment.type";
+import ApartmentModal from "./apartment/apartmentModal";
+import { getFormData } from "#/utils/getFormData";
+import { requestAddNewApartment } from "#/api/requestAddNewApartment";
 
 const Apartments = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [activeIcon, setActiveIcon] = useState({
-    all: false,
+  const [activeIcon, setActiveIcon] = useState<ActiveIcon>({
+    all: true,
     noDiscount: false,
     withDiscount: false,
   });
-  const [activeEditModal, setActiveEditModal] = useState<boolean>(false);
+  const filterValues = {
+    discount: searchParams.get("discount") ?? "all",
+    sortBy: searchParams.get("sortBy") ?? "name-asc",
+  };
+  const [activeModal, setActiveModal] = useState<boolean>(false);
 
-  const { data: apartments, isLoading } = useQuery({
-    queryKey: ["apartments"],
-    queryFn: () => requestApartments(),
+  const {
+    data: apartments,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["apartments", filterValues],
+    queryFn: () => requestApartments(filterValues),
+    placeholderData: keepPreviousData,
   });
   if (isLoading) return;
 
@@ -32,13 +45,16 @@ const Apartments = () => {
     searchParams.set("sortBy", option);
     setSearchParams(searchParams);
   };
+  const handleDiscount = (discount: string, id: string) => {
+    setDiscount(discount);
+    handleActiveIcon(id);
+  };
   const setDiscount = (discount: string) => {
     searchParams.set("discount", discount);
     setSearchParams(searchParams);
   };
 
-  const handleActiveIcon = (e: React.ChangeEvent<HTMLButtonElement>) => {
-    const name = e.target.name;
+  const handleActiveIcon = (name: string) => {
     setActiveIcon((prev) => {
       const active = Object.fromEntries(
         Object.entries(prev).map(([icon]) =>
@@ -48,18 +64,18 @@ const Apartments = () => {
       return active as typeof activeIcon;
     });
   };
-
-  const handleEditApartment = async (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const id = Number(e.target.id);
-    const formData = Object.fromEntries(new FormData(e.target));
-    const data = { id, ...formData };
-    await requestEditApartment(data);
-    handleActiveEditModal();
+  const handleAddNewApartment = async (
+    e: React.ChangeEvent<HTMLFormElement>,
+  ) => {
+    const data = getFormData(e);
+    await requestAddNewApartment(data);
+    handleActiveModal();
+    refetch();
   };
-  const handleActiveEditModal = () => {
-    setActiveEditModal(prev => !prev);
-  }
+
+  const handleActiveModal = () => {
+    setActiveModal((prev) => !prev);
+  };
 
   return (
     <div className="apartments">
@@ -67,13 +83,15 @@ const Apartments = () => {
         <h1>All apartments</h1>
         <menu>
           {aparmnetsDiscount.map((discount) => {
-            const { name, content } = discount;
+            const { name, content, id } = discount;
             return (
               <Btn
+                type="button"
                 key={name}
+                onClick={() => handleDiscount(name, id)}
                 variation="ghost"
                 size="md"
-                active={`${activeIcon && activeIcon[name] && "act"}`}
+                active={`${activeIcon[id as keyof ActiveIcon] && "act"}`}
                 name={name}
               >
                 {content}
@@ -81,9 +99,9 @@ const Apartments = () => {
             );
           })}
         </menu>
-        <Select options={apartmentsSort} size="md" />
+        <Select onChange={setSort} options={apartmentsSort} size="md" />
       </div>
-      <div className="apartments-table">
+      <table className="apartments-table">
         <thead className="apartments-table-header">
           <tr>
             <th className="apartment-th">Apartment</th>
@@ -92,22 +110,30 @@ const Apartments = () => {
             <th className="discount-th">Discount</th>
           </tr>
         </thead>
-
-        <table className="apartments-table-content">
-          {apartments?.map((apartment) => {
-            return (
-              <Apartment
-                key={apartment.id}
-                apartment={apartment}
-                handleEditApartment={handleEditApartment}
-                activeEditModal={activeEditModal}
-                handleActiveEditModal={handleActiveEditModal}
-              />
-            );
-          })}
-        </table>
-      </div>
-      <Btn variation="ghost" size="md">
+        {apartments?.map((apartment: ApartmentType, index: number) => {
+          return (
+            <Apartment
+              key={apartment.id}
+              isLast={index === apartments.length - 1}
+              apartment={apartment}
+              refetch={refetch}
+            />
+          );
+        })}
+      </table>
+      {activeModal && (
+        <ApartmentModal
+          activeModal={activeModal}
+          handleActiveModal={handleActiveModal}
+          handleApartmentData={handleAddNewApartment}
+        />
+      )}
+      <Btn
+        onClick={handleActiveModal}
+        type="button"
+        variation="primary"
+        size="lg"
+      >
         Add new apartment
       </Btn>
     </div>
