991c96a4aa451b5c2a5f0cf9fe9e899a71ac9b1e
Bernard Krehula <krehula.bernard456@gmail.com>
2026-07-01 21:52:53 +0200

    feat: add map on columns

--- src/components/ui/table/TableData/index.tsx
+++ src/components/ui/table/TableData/index.tsx
@@ -1,10 +1,17 @@
-import './index.css';
+import "./index.css";
 
-const TableData = () => {
-    return(
-        <td>
-
-        </td>
-    )
-}
+const TableData = ({ row, columns }) => {
+    console.log(row)
+  return (
+    <tr>
+      {columns.map((col) => {
+        return (
+          <td key={col.key} className={col.className}>
+            {col.render ? col.render(row) : row[col.key]}
+          </td>
+        );
+      })}
+    </tr>
+  );
+};
 export default TableData;
